bd29175be9441a29f69c2f1d986138546ef9e131
JLTDigital <mrjltaft@gmail.com>
2021-01-25 22:11:32 +0000

    Update

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container,  } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+  <header>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">CybrShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav className="ml-auto">
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>  Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign in</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </header>
+  )
+}
+
+export default Header
