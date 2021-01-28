77e90b8be93a4c31fed435ac0dabada8fe90e9b6
JLTDigital <mrjltaft@gmail.com>
2021-01-28 22:17:58 +0000

    Update, Backend set up

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import axios from 'axios'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id) 
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
 
   return (
     <>
-     <Link className='btn btn-light my-3' to='/'>Back</Link> 
+     <Link className='btn btn-light my-3' to='/latest-products'>Back</Link> 
      <Row>
        <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
