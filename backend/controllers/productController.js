36ed0b5ac92cf6e83562cc2d0258484e2929f5e8
JLTDigital <mrjltaft@gmail.com>
2021-02-05 22:55:30 +0000

    Product Route Controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,71 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/Productmodel.js'
+
+// @desc - Fetch latest products
+// @route - GET /api/latest/products
+// @access - Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ category: 'latest'})
+
+  res.json(products)
+})
+
+// @desc - Fetch computer & laptop products
+// @route - GET /api/computers-laptops
+// @access - Public
+const getComputerLaptopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ category: 'computers/laptops'})
+
+  res.json(products)
+})
+
+// @desc - Fetch retro products
+// @route - GET /api/retro
+// @access - Public
+const getRetroProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ category: 'retro'})
+
+  res.json(products)
+})
+
+// @desc - Fetch smartphone products
+// @route - GET /api/retro
+// @access - Public
+const getSmartphoneProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ category: 'smartphones'})
+
+  res.json(products)
+})
+
+// @desc - Fetch VR products
+// @route - GET /api/retro
+// @access - Public
+const getVrProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ category: 'VR'})
+
+  res.json(products)
+})
+
+// @desc - Fetch a single product
+// @route - GET /api/products/:id
+// @access - Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+    
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export {
+  getProducts,
+  getRetroProducts,
+  getComputerLaptopProducts,
+  getSmartphoneProducts,
+  getVrProducts,
+  getProductById
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,68 +1,19 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/Productmodel.js'
-
 const router = express.Router()
+import { 
+  getComputerLaptopProducts, 
+  getProductById, 
+  getProducts, 
+  getRetroProducts, 
+  getSmartphoneProducts, 
+  getVrProducts 
+} from '../controllers/productController.js'
 
-// @desc - Fetch latest products
-// @route - GET /api/latest/products
-// @access - Public
-router.get('/latest/products', asyncHandler(async (req, res) => {
-  const products = await Product.find({ category: 'latest'})
-
-  res.json(products)
-}))
-
-// @desc - Fetch computer & laptop products
-// @route - GET /api/computers-laptops
-// @access - Public
-router.get('/computers-laptops', asyncHandler(async (req, res) => {
-  const products = await Product.find({ category: 'computers/laptops'})
-
-  res.json(products)
-}))
-
-// @desc - Fetch retro products
-// @route - GET /api/retro
-// @access - Public
-router.get('/retro', asyncHandler(async (req, res) => {
-  const products = await Product.find({ category: 'retro'})
-
-  res.json(products)
-}))
-
-// @desc - Fetch smartphone products
-// @route - GET /api/retro
-// @access - Public
-router.get('/smartphones', asyncHandler(async (req, res) => {
-  const products = await Product.find({ category: 'smartphones'})
-
-  res.json(products)
-}))
-
-// @desc - Fetch VR products
-// @route - GET /api/retro
-// @access - Public
-router.get('/vr', asyncHandler(async (req, res) => {
-  const products = await Product.find({ category: 'VR'})
-
-  res.json(products)
-}))
-
-// @desc - Fetch a single product
-// @route - GET /api/products/:id
-// @access - Public
-router.get('/product/:id', asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id)
-
-  if (product) {
-    res.json(product)
-    
-  } else {
-    res.status(404)
-    throw new Error('Product not found')
-  }
-
-}))
+router.route('/latest/products').get(getProducts)
+router.route('/computers-laptops').get(getComputerLaptopProducts)
+router.route('/retro').get(getRetroProducts)
+router.route('/smartphones').get(getSmartphoneProducts)
+router.route('/vr').get(getVrProducts)
+router.route('/product/:id').get(getProductById)
 
 export default router
