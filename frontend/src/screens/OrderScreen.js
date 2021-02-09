c7514965d96595d9c0d5b2e7d9e32a0b88f3e9a6
JLTDigital <mrjltaft@gmail.com>
2021-02-09 19:44:44 +0000

    Update

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,10 +11,15 @@ import { getOrderDetails } from '../actions/orderActions'
 const OrderScreen = ({ match }) => {
   const orderId = match.params.id
 
+  const [sdkReady, setSdkReady] = useState(false)
+
   const dispatch = useDispatch()
 
   const orderDetails = useSelector(state => state.orderDetails)
   const { order, loading, error } = orderDetails
+
+  const orderPay = useSelector(state => state.orderPay)
+  const { loading: loadingPay, success: successPay} = orderPay
 
   if (!loading) {
     const addDecimals = (num) => {
@@ -23,10 +29,32 @@ const OrderScreen = ({ match }) => {
   }
 
   useEffect(() => {
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
+    }
+
+    if (!order || successPay) {
+      dispatch(getOrderDetails(orderId))
+    } else if(!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript()
+      } else {
+        setSdkReady(true)
+      }
+    }
+
     if (!order || order._id !== orderId) {
       dispatch(getOrderDetails(orderId))
     }
-  }, [match, dispatch, order, orderId])
+  }, [match, dispatch, order, orderId, successPay])
 
   return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <>
     <h1>Order {orderId}</h1>
